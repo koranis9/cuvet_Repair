852e8d215eff5d92d449cd224a16a317c7395ae4
koranis9 <koranis9@gmail.com>
2026-06-05 15:47:02 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,21 @@
-const CACHE = 'repair-v2';
-// ปรับเส้นทางให้ถูกต้องตามโครงสร้าง GitHub Pages
+const CACHE = 'repair-v3';
 const FILES = [
   '/cuvet_Repair/',
   '/cuvet_Repair/index.html',
   '/cuvet_Repair/manifest.json',
   '/cuvet_Repair/icon-192.png'
 ];
-
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)))
-);
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
 self.addEventListener('fetch', e =>
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
 );
