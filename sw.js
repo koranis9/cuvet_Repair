85135286465dc3346ab60e97593aa431de24d0ce
koranis9 <koranis9@gmail.com>
2026-06-05 18:07:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'repair-v8';
+const CACHE = 'repair-v9';
 const FILES = ['/cuvet_Repair/','/cuvet_Repair/index.html','/cuvet_Repair/manifest.json','/cuvet_Repair/icon-192.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
